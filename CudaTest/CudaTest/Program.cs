2717452c8795c4cef109ffc63ab699e18e659495
Ismael Núñez <ismael.inc@gmail.com>
2018-10-25 17:25:32 +0200

    created repository

---
+++ CudaTest/CudaTest/Program.cs
@@ -0,0 +1,56 @@
+﻿using Hybridizer.Runtime.CUDAImports;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CudaTest
+{
+    class Program
+    {
+        public static readonly int MAX_PARALELLISM = 2000;
+        public static readonly int TEST_COUNT = 2000000;
+
+        [EntryPoint]
+        public static void Run(double[] a, double[] b)
+        {
+            Parallel.For(
+                0,
+                a.Length-1,
+                new ParallelOptions { MaxDegreeOfParallelism = MAX_PARALELLISM},
+                i => {
+                    double auxA = a[i];
+                    a[i] += Math.Atan( Math.Log10(a[i]) + Math.Log10(b[i]) );
+                    b[i] = Math.Atan2(Math.Log10(auxA), Math.Log10(b[i]));
+                });
+        }
+
+        static void Main(string[] args)
+        {
+            double[] a = new double[TEST_COUNT], b = new double[TEST_COUNT];
+            Random generator = new Random();
+            for (int i = 0; i < TEST_COUNT; i++)
+            {
+                a[i] = generator.Next(int.MaxValue);
+                Thread.Sleep(3);
+                b[i] = generator.Next(int.MaxValue);
+            }
+
+            cudaDeviceProp prop;
+            cuda.GetDeviceProperties(out prop, 0);
+            //if .SetDistrib is not used, the default is .SetDistrib(prop.multiProcessorCount * 16, 128)
+            HybRunner runner = HybRunner.Cuda();
+
+            // create a wrapper object to call GPU methods instead of C#
+            dynamic wrapped = runner.Wrap(new Program());
+
+            Console.Out.WriteLine(String.Format("[{0}] Started heavy load task."), DateTime.Now.ToString());
+
+            wrapped.Run(a, b);
+
+            Console.Out.WriteLine(String.Format("[{0}] Finished heavy load task."), DateTime.Now.ToString());
+            Console.ReadKey();
+        }
+
+    }
+
+}
---
+++ CudaTest/CudaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using Hybridizer.Runtime.CUDAImports;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CudaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CudaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ec627420-4759-4f3a-a04b-07b316db01c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// allow parameterless constructor for HybRunner
+[assembly: HybRunnerDefaultSatelliteName("CudaTest_CUDA.dll")]
